29d6466dea62300ee9f8714232fb72be3b57b684
Igor <igglike@gmail.com>
2023-11-11 18:10:34 +0600

    Сделал вложенные марщруты

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import MainPage from "./pages/MainPage";
 import PostsPage from "./pages/PostsPage";
 import OnePostPage from "./pages/OnePostPage";
-import Navbar from './components/Navbar';
+import Layout from "./components/Layout";
 
 function App() {
-  return ( <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<MainPage />} />
-      <Route path='/posts' element={<PostsPage />} />
-      <Route path='/posts/:id' element={<OnePostPage />} />
-    </Routes>
-  </>);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="posts" element={<PostsPage />} />
+          <Route path="posts/:id" element={<OnePostPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,6 +1,23 @@
+import { useState, useEffect } from "react"
+
 const PostsPage = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+  }, [])
+
   return (
-    <div>PostsPage</div>
+    <div>
+      PostsPage
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 export default PostsPage
